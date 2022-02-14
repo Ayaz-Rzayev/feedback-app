14b8f78d3ba8dcac6f874f61e37ceed86cb7b5fd
Ayaz-Rzayev <rzayev.ayaz198@gmail.com>
2022-02-14 22:36:05 +0400

    Feedback delete logic added

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./FeedbackItem.module.css";
 import Card from "./UI/Card";
+import {FaTimes} from "react-icons/fa"
 
-const FeedbackItem = ({rating, text }) => {
+const FeedbackItem = ({id,rating, text, onDelete }) => {
   return (
     <Card>
       <div className={styles["card__num"]}>{rating}</div>
+      <button className={styles.close} onClick={()=>onDelete(id)}>
+        <FaTimes color="purple"/>
+      </button>
       <p className={styles["card__text"]}>{text}</p>
     </Card>
   );
--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -3,9 +3,25 @@ import FeedbackItem from "./FeedbackItem";
 import FeedbackData from "../data/FeedbackData";
 
 const FeedbackList = () => {
-  //const [feedback, setFeedback] = useState(FeedbackData)
-  return FeedbackData.map((item) => {
-    return <FeedbackItem key={item.id} rating={item.rating} text={item.text} />;
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const feedbackDeleteHandler = (id) => {
+    if(window.confirm('Are you sure you want to delete?')){
+      let newFeedback = feedback.filter((item) => item.id !== id);
+      setFeedback(newFeedback);
+    }
+  };
+
+  return feedback.map((item) => {
+    return (
+      <FeedbackItem
+        key={item.id}
+        id={item.id}
+        rating={item.rating}
+        text={item.text}
+        onDelete={feedbackDeleteHandler}
+      />
+    );
   });
 };
 
