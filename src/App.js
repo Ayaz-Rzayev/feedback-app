0ef3147d6fe9647b482448e6cf7cbb47bafbc7ae
Ayaz-Rzayev <rzayev.ayaz198@gmail.com>
2022-02-22 11:59:00 +0400

    AboutPage route added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import styles from "./App.module.css";
 import FeedbackList from "./components/FeedbackList";
 import FeedbakStats from "./components/FeedbackStats";
 import Header from "./components/UI/Header";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackData from "./data/FeedbackData";
+import AboutPage from "./components/pages/AboutPage";
 
 function App() {
   const [feedbacks, setFeedback] = useState(FeedbackData);
@@ -17,20 +19,34 @@ function App() {
   };
 
   const feedbackAddHandler = (newFeedback) => {
-    newFeedback.id = uuidv4()
+    newFeedback.id = uuidv4();
     let newFeedbackList = [newFeedback, ...feedbacks];
     setFeedback(newFeedbackList);
   };
 
   return (
-    <Fragment>
+    <Router>
       <Header text="Feedback UI" />
       <div className={styles.container}>
-        <FeedbackForm onAdd={feedbackAddHandler} />
-        <FeedbakStats feedbacks={feedbacks} />
-        <FeedbackList feedbacks={feedbacks} onDelete={feedbackDeleteHandler} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <Fragment>
+                <FeedbackForm onAdd={feedbackAddHandler} />
+                <FeedbakStats feedbacks={feedbacks} />
+                <FeedbackList
+                  feedbacks={feedbacks}
+                  onDelete={feedbackDeleteHandler}
+                />
+              </Fragment>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
-    </Fragment>
+    </Router>
   );
 }
 
---
+++ src/components/pages/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Card from "../UI/Card"
+
+
+const AboutPage = () => {
+  return (
+    <Card>
+      <h1>About this project</h1>
+      <p>Simple feedback app build with React to practice (based on Brad Traversy's course on React)</p>
+    </Card>
+  )
+}
+
+export default AboutPage
